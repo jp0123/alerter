b2291c03a2b758b237d77e3c573531ad68aa907b
Joseph Park <joseph.thomas.park@gmail.com>
2018-08-29 14:27:58 +1000

    Minor refactoring and tidying up of code

--- orders.js
+++ orders.js
@@ -1,5 +1,5 @@
 require('dotenv-safe').config();
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 const url = `${process.env.URL}${process.env.BRAND}`;
 
@@ -10,7 +10,7 @@ const fetch_object = {
         "Content-Type": "application/json; charset=utf-8",
         "Cookie": `access_token=${process.env.ACCESS_TOKEN_COOKIE}`
     }
-}
+};
 
 // API | Fetch 10 of the most recent orders
 const ordersAPI = fetch(url, fetch_object)
@@ -37,23 +37,25 @@ ordersAPI
     })
     // Delta check | Determine how long it has been since these orders been purchased or processed
     .then(orders => {
+        let successfulOrdersWithinTimeFrameCounter = 0;
         const currentTime = Date.now();
         const timeFrame = Number(process.env.TIME_FRAME); // 15 minutes in milliseconds
         const timeFrameInMinutes = Math.round((timeFrame / 1000 / 60));
-        let successfulOrdersWithinTimeFrameCounter = 0;
         // Check order | Has it been ordered within the specified time frame?
         orders.forEach(order => {
             const orderProcessed = Date.parse(order.created_at);
             const delta = currentTime - orderProcessed; // Milliseconds
             if (timeFrame > delta) { // Successful orders within the last timeFrame
                 successfulOrdersWithinTimeFrameCounter += 1;
-            }
+            };
         })
         // Diagnosis | Are successful orders coming in the specified time frame?
         if (successfulOrdersWithinTimeFrameCounter === 0) {
-            console.log(`WARNING: No orders have been successfully purchased or processed for at least ${timeFrameInMinutes} minutes!`);
+            const warningNotification = `WARNING: No orders have been successfully purchased or processed for at least ${timeFrameInMinutes} minutes!`;
+            console.log(warningNotification);
             process.exit(1);
         } else {
-            console.log(`SUCCESS: ${successfulOrdersWithinTimeFrameCounter} of the last 10 orders have been successfully purchased or processed within the last ${timeFrameInMinutes} minutes.`)
+            const successNotification = `SUCCESS: ${successfulOrdersWithinTimeFrameCounter} of the last 10 orders have been successfully purchased or processed within the last ${timeFrameInMinutes} minutes.`;
+            console.log(successNotification);
         }
     });
